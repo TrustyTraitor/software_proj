8cb2a5e6389b69176cc9021f48f4526afc7f98d9
Connor Bass <connorbass15@gmail.com>
2023-04-08 03:03:10 -0400

    edit personal information

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from Entities.User import User, load_users
 # Boundary and Control Classes
 from ctrl_login import ctrl_Login
 from ctrl_logout import ctrl_Logout
+from ctrl_edit_personal_information import ctrl_Edit_Personal_Information
 from ctrl_view_students import ctrl_Admin_View_Students
 from ctrl_view_courses import ctrl_View_Courses
 from ctrl_register_class import ctrl_Student_Register_Class
@@ -35,6 +36,7 @@ def main():
         print("3. View Registered Classes")
         print("4. View Students")
         print("5. Logout")
+        print("6. Edit Personal Information")
         selection = int(input("Enter a selection: "))
         if selection == 1:
             ctrl_View_Courses.view(courses)
@@ -47,6 +49,8 @@ def main():
         elif selection == 5:
             ctrl_Logout.Logout(current_user)
             break
+        elif selection == 6:
+            ctrl_Edit_Personal_Information.pickEdit(current_user)
         selection = 0
 
 
